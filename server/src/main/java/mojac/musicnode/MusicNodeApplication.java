f919272e189347867a3e34b9a1ea7c0b4f5662b0
hhyuw <high2092@gmail.com>
2023-03-31 20:54:37 +0900

    Initialize Spring Boot Project

---
+++ server/src/main/java/mojac/musicnode/MusicNodeApplication.java
@@ -0,0 +1,13 @@
+package mojac.musicnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicNodeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/mojac/musicnode/MusicNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package mojac.musicnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
