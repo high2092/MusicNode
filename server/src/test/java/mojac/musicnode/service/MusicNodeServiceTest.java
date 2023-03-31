69bb37a7793bbff56ad09b6fad803ba58d184466
hhyuw <high2092@gmail.com>
2023-04-01 00:59:51 +0900

    [BE] test: MusicNode 서비스 통합 테스트 작성

---
+++ server/src/test/java/mojac/musicnode/service/MusicNodeServiceTest.java
@@ -0,0 +1,52 @@
+package mojac.musicnode.service;
+
+import mojac.musicnode.domain.Category;
+import mojac.musicnode.domain.Music;
+import mojac.musicnode.domain.MusicNode;
+import mojac.musicnode.repository.CategoryRepository;
+import mojac.musicnode.repository.MusicNodeRepository;
+import mojac.musicnode.repository.MusicRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MusicNodeServiceTest {
+
+    @Autowired CategoryService categoryService;
+    @Autowired MusicService musicService;
+    @Autowired MusicNodeService musicNodeService;
+
+    @Test
+    @Rollback(false)
+    public void 뮤직_노드_생성_통합_테스트() {
+        // g
+        Music music = new Music("아아", "12312");
+
+        Category category = new Category("카테고리1", Color.black);
+        music.addCategory(category);
+
+        musicService.saveMusic(music);
+        categoryService.saveCategory(category);
+
+        // w
+        MusicNode musicNode = new MusicNode(music);
+        Long savedId = musicNodeService.saveMusicNode(musicNode);
+
+        // t
+        MusicNode foundNode = musicNodeService.findOne(savedId);
+
+        assertThat(foundNode.getMusic().getCategories().size()).isEqualTo(1);
+        assertThat(foundNode.getMusic()).isEqualTo(music);
+    }
+
+}
