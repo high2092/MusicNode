33e8496688d8e3f2f075816b96845a5cf584469c
hhyuw <high2092@gmail.com>
2023-04-01 02:51:46 +0900

    [BE] chore: CORS 설정

--- server/src/main/java/mojac/musicnode/MusicNodeApplication.java
+++ server/src/main/java/mojac/musicnode/MusicNodeApplication.java
@@ -2,12 +2,28 @@ package mojac.musicnode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MusicNodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicNodeApplication.class, args);
+	}
+
+	final static String CLIENT_URL = "http://localhost:3000";
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(CLIENT_URL)
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+			}
+		};
 	}
 
 }
